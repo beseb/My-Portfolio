397e1b9618be4ea9162b0eb1e6976eb70586d32c
Sebastien Escudero <sebastien.escudero@gmail.com>
2023-11-20 12:20:12 +0100

    component card animation & css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,60 @@
 /* eslint-disable react/prop-types */
+import React, { useState } from 'react';
 import '../styles/main.scss';
 
 function Card({ datas }) {
+  const image = datas.images[0];
+  const [isHovered, setIsHovered] = useState(false);
+
+  const onMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const onMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const overlayStyle = {
+    backgroundColor: 'rgba(128, 128, 128, 0.3)',
+    width: isHovered ? '100%' : '0%',
+    transition: 'width 0.5s ease-in-out',
+    height: '20%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    wordBreak: 'none',
+    overflow: 'visible',
+    
+  };
+  const titleStyle = {
+    whiteSpace: 'nowrap',
+  };
+  const imageContainerStyle = {
+    position: 'relative',
+    overflow: 'hidden',
+    height: '80%',
+    width: '100%',
+  };
+
+  const imageStyle = {
+    width: '100%',
+    height: '100%',
+    transform: isHovered ? 'scale(1.03)' : 'none',
+    transition: 'all 0.1s ease-in-out',
+  };
+
   return (
-    <a className='card_container'>
-      <h3 className='--title'>{datas.title}</h3>
-      <div className='--images_container'>{datas.images[0]}</div>
-      <p className='--starDate italic'>{datas.startDate}</p>
-      <button href={datas.url} target='blank'>
-        {datas.cta}
-      </button>
+    <a
+      className={`card_container ${datas.id}`}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      <div className='--images_container' style={imageContainerStyle}>
+        <img src={image} alt='' style={imageStyle} />
+      </div>
+      <div className='_overlay' style={overlayStyle}>
+        <h3 className='--title' style={titleStyle}>{datas.title}</h3>
+      </div>
     </a>
   );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,7 @@ import '../styles/main.scss';
 import '../styles/components/projects.scss';
 import Card from './Card';
 import Modal from './Modal';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function Projects({ datas }) {
   const [openProjectId, setOpenProjectId] = useState(null);
@@ -16,10 +16,26 @@ function Projects({ datas }) {
     setOpenProjectId(null); // Réinitialiser l'ID du projet ouvert
   };
 
+  // // Fetch pour récuperer les images 
+  // const [imagesURL, setImagesURL] = useState([]);
+  // useEffect(() => {
+  //   // Récupérer le fichier JSON contenant les données des projets
+  //   fetch('../assets/datas/res_primaryLanguage.json')
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       // Mettre à jour l'état avec les données des projets
+  //       setImagesURL(data);
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error fetching JSON:', error);
+  //     });
+  // }, []);
+
   const projects = datas.projects.map((project, index) => {
+
     return (
       <li key={index} id='project-card' onClick={() => handleCardClick(index)}>
-        <Card datas={project} />
+        <Card datas={project}  />
         <Modal isOpen={openProjectId === index} setIsOpen={handleCloseModal} project={project} />
       </li>
     );
