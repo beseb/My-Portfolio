9eef8b3aca25be3c2ca4041ba8119cbebffb06bd
Sebastien Escudero <sebastien.escudero@gmail.com>
2023-11-21 15:15:51 +0100

    fixed some css on carousel, need more fixing

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,24 +3,25 @@ import { width } from '@mui/system';
 import Carousel from 'react-material-ui-carousel';
 
 function ImageCarousel({ data }) {
-    const items = data.images;
-   
-    return (
-      <div className="carouselWrapper">
-        <Carousel className='carousel'
-          autoPlay={true}
-          interval={3000}
-          cycleNavigation={true}
-          navButtonsAlwaysVisible={true}
-        >
-          {items.map((item, i) => (
-            <Item key={i} item={item} />
-          ))}
-        </Carousel>
-      </div>
-    );
-  }
+  const items = data.images;
+
+  return (
+    <div className='carouselWrapper'>
+      <Carousel
+        className='carousel'
+        autoPlay={true}
+        interval={3000}
+        cycleNavigation={true}
+        navButtonsAlwaysVisible={true}
+      >
+        {items.map((item, i) => (
+          <Item key={i} item={item} className='carouselItem' />
+        ))}
+      </Carousel>
+    </div>
+  );
+}
 export default ImageCarousel;
 function Item({ item }) {
-  return <img src={item} className='carouselImage'/>;
+  return <img src={item} className='carouselImage' />;
 }
