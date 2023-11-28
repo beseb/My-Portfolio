b4bf1687840e08bc366bd536a73b93f73b428ac3
Sebastien Escudero <sebastien.escudero@gmail.com>
2023-11-28 14:50:15 +0100

    deleted image component on banner section

--- src/sections/Banner.jsx
+++ src/sections/Banner.jsx
@@ -2,63 +2,28 @@
 import Typical from "react-typical";
 import "../styles/main.scss";
 import seaLogo from "/images/SEA-logo2.png";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { LangContext } from "../utils/LangContext";
 import { useContext } from "react";
-import styled from "styled-components";
-import pic_list from "../assets/datas/pic_list.json";
-import ImageComponent from "../components/ImageComponent";
-import ImageComponent2 from "../components/ImageComponent2";
+
 function Banner() {
   const { datas } = useContext(LangContext);
 
-  const BackDiv = styled.div`
-    width: 30%;
-    height: 55%;
-    border-radius: 15px;
-    opacity: 0.7;
-    position: absolute;
-    top: 500px;
-    left: 110px;
-    @media screen and (max-width: 768px){
-      display:none;
-    }
-  `;
-  const BackDiv2 = styled.div`
-    width: 30%;
-    height: 55%;
-    border-radius: 15px;
-    opacity: 0.7;
-    position: absolute;
-    top: 500px;
-    right: 110px;
-    @media screen and (max-width: 768px){
-      display:none;
-    }
-  `;
-
   return (
     <div className="banner-section " role="banner">
-      <BackDiv>
-        <ImageComponent />
-      </BackDiv>
-      <BackDiv2>
-        <ImageComponent2 />
-      </BackDiv2>
       <div className="content">
-      <img
-        src={seaLogo}
-        alt="Logo de SEA Web-Studio"
-        className="unselectable"
-        role="img"
-        aria-label="Logo de SEA Web-Studio"
-        height="500px"
-        width="500px"
-      />
+        <img
+          src={seaLogo}
+          alt="Logo de SEA Web-Studio"
+          className="unselectable"
+          role="img"
+          aria-label="Logo de SEA Web-Studio"
+          height="500px"
+          width="500px"
+        />
         <h1 style={{ visibility: "hidden", height: "0px", width: "0px" }}>
           SEA Web-Studio
         </h1>
-        <Typical 
+        <Typical
           steps={[
             ". . .",
             1000,
@@ -72,7 +37,6 @@ function Banner() {
           loop={Infinity}
           wrapper="h2"
           className="typical"
-          
           aria-label="Texte animé : développeur web, designer et créateur"
         />
         {/* <a href="#about" className="btn CTA">
